b7bcf4854be2b16e012cf03e2b652450cc74a198
w.benarto <w.benarto@gmail.com>
2019-01-04 17:20:14 -0800

    &

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,22 @@
 const express = require('express');
-const burger = require('./models/burger')
+const router = express.Router();
+
+// Import the model to use its database functions
+const burger = require('../models/burger');
+
+// Create routes 
+router.get('/', function(req, res) {
+    burger.selectAll(function(data) {
+        let hbsObject = {
+            burgers: data
+        };
+
+        res.render('index', hbsObject)
+    });
+});
+
+
+//Not today.... 
+
+// Export routes for server.js
+module.exports = router;
