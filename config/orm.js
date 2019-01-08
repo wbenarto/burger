6c90ba3b705075de14c18c041a274961f33532a0
w.benarto <w.benarto@gmail.com>
2019-01-07 21:13:34 -0800

    orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,28 @@
+// Import MySQL connection
 const connection = require("./connection");
 
+// Helper function for generating MySQL syntax
+const printQuestionMarks = (num) => {
+    let arr = [];
 
-let orm = {
+    for (var i = 0; i < num; i++) {
+        arr.push("?");
+    };
+
+    return arr.toString();
+};
+
+const objToSql = (ob) => {
+    let arr = [];
+
+    for (var key in ob) {
+        arr.push(key + "=" + ob[key]);
+    };
+
+    return arr.toString();
+}
+ 
+const orm = {
     selectAll: () => {},
     insertOne: () => {},
     updateOne: () => {}
